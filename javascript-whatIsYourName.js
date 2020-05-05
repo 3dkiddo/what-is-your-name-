b8814a905f9972510fb4877f0274bde94665cdaa
3dkiddo <64550267+3dkiddo@users.noreply.github.com>
2020-05-05 15:18:07 +0300

    Add files via upload

---
+++ javascript-whatIsYourName.js
@@ -0,0 +1,19 @@
+// var username = prompt("what is your username?");
+
+// var usernameFirstLetter = username.slice(0,1);
+// var usernameFirstLetter = usernameFirstLetter.toUpperCase();
+// var usernameRestOfAName = username.slice(1, username.length);
+// var usernameRightFinal = usernameRestOfAName.toLowerCase();
+// var usernameFinal = (usernameFirstLetter + usernameRightFinal);
+
+// alert("Hello " + usernameFinal);
+
+var username = prompt("Скажи свое имя плс");
+
+var usernameFirstLetter = username.slice(0,1);
+var usernameFirstLetter = usernameFirstLetter.toUpperCase();
+var usernameRestOfAName = username.slice(1, username.length);
+var usernameRightFinal = usernameRestOfAName.toLowerCase();
+var usernameFinal = (usernameFirstLetter + usernameRightFinal);
+
+alert("Ну здравствуй, " + usernameFinal);
